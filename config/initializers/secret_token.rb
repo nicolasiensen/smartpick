69a432055093836530b0c80fe42b3218c1b051b4
Nícolas Iensen <nicolas@iensen.me>
2013-11-02 15:55:00 -0200

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartpick::Application.config.secret_key_base = '1b1e2bed0aa29e85d4a68adfc299eb80f72c76d128b19659669cbb733695335f921f9b9b94d4adcb9fde63edf516db9430d88ffc684479e1140d9d0c3d62d33f'
