7890e5bcb5df46bdee25c91a59be5ce7793cfe8c
Nícolas Iensen <nicolas@iensen.me>
2013-11-30 19:33:35 -0200

    Remove test files

--- app/models/reference.rb
+++
@@ -1,4 +0,0 @@
-class Reference < ActiveRecord::Base
-  validates :price, :model_id, :date, presence: true
-  validates :model_id, uniqueness: { scope: :date }
-end
--- spec/models/reference_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Reference do
-  it { should validate_presence_of :price }
-  it { should validate_presence_of :model_id }
-  it { should validate_presence_of :date }
-end
