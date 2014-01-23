f826043b38f7347a833d3952b75900571efa6d76
Nícolas Iensen <nicolas@iensen.me>
2014-01-23 16:02:31 -0200

    Fix charts for those cars that have less than 4 models

--- app/assets/javascripts/compares.js
+++ app/assets/javascripts/compares.js
@@ -1,36 +1,48 @@
 $(function(){
-  google.setOnLoadCallback(drawChart);
-  var json = $("#col_chart_custom_tooltip").data('json');
-  var cars = json['cars'];
+  if($("body.compares.show").length){
+    google.setOnLoadCallback(drawChart);
+    var json = $("#col_chart_custom_tooltip").data('json');
+    var cars = json['cars'];
 
-  function drawChart() {
-    var dataTable = new google.visualization.DataTable();
-    dataTable.addColumn('string', 'Referencia');
+    function drawChart() {
+      var dataTable = new google.visualization.DataTable();
+      dataTable.addColumn('string', 'Referencia');
 
-    for(var i = 0; i < cars.length; i++){
-      dataTable.addColumn('number', cars[i]['name']);
-      dataTable.addColumn({type: 'string', role: 'tooltip'});
+      for(var i = 0; i < cars.length; i++){
+        dataTable.addColumn('number', cars[i]['name']);
+        dataTable.addColumn({type: 'string', role: 'tooltip'});
+      }
+
+      for(var i = 0; i < 4; i++){
+        dataTable.addRow([
+                         i.toString(), 
+                         getModelValue(i, cars[0]), tooltipFor(i, cars[0]), 
+                         getModelValue(i, cars[1]), tooltipFor(i, cars[1]), 
+                         getModelValue(i, cars[2]), tooltipFor(i, cars[2])
+        ]);
+      }
+
+      var options = { legend: 'none', pointSize: 5 };
+      var chart = new google.visualization.AreaChart(document.getElementById('col_chart_custom_tooltip'));
+      chart.draw(dataTable, options);
+    };
+
+    function getModelValue(year, car){
+      if(car['models'][year])
+        return car['models'][year]['value'];
+      else
+        return 0;
     }
 
-    for(var i = 0; i < 4; i++){
-      dataTable.addRow([
-                       i.toString(), 
-                       valueFor(i, cars[0]), tooltipFor(i, cars[0]), 
-                       valueFor(i, cars[1]), tooltipFor(i, cars[1]), 
-                       valueFor(i, cars[2]), tooltipFor(i, cars[2])
-      ]);
+    function getModelName(year, car){
+      if(car['models'][year])
+        return car['models'][year]['name'];
+      else
+        return "Sem nome";
     }
 
-    var options = { legend: 'none', pointSize: 5 };
-    var chart = new google.visualization.AreaChart(document.getElementById('col_chart_custom_tooltip'));
-    chart.draw(dataTable, options);
-  };
-
-  function valueFor(year, car){
-    return car['models'][year]['value'];
-  };
-
-  function tooltipFor(year, car){
-    return car['models'][year]['name'].concat(": R$").concat(car['models'][year]['value'].toString());
-  };
+    function tooltipFor(year, car){
+      return getModelName(year, car).concat(": R$").concat(getModelValue(year, car).toString());
+    };
+  }
 });
