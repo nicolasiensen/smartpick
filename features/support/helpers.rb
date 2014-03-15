ad91754e51b9b0f7869bd6ee6cb55617114eb857
Nícolas Iensen <nicolas@engage.is>
2014-03-15 15:01:47 -0300

    Fix tests

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -6,8 +6,8 @@ end
 
 def to_element string
   return 'form.new_compare'                           if string == 'the comparison form'
-  return 'form.new_compare #error_explanation'        if string == 'the comparison form error message'
-  return 'model_name_1'                               if string == 'the first comparison car field'
-  return 'model_name_2'                               if string == 'the second comparison car field'
+  return '.car-name-1-field small.error'              if string == 'the comparison form error message'
+  return 'car_name_1'                                 if string == 'the first comparison car field'
+  return 'car_name_2'                                 if string == 'the second comparison car field'
   raise "I don't know what '#{string}' means"
 end
